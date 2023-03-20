6418ac2fc7c6d7ca0358abe7569497e201b9c0d3
José Augusto <josefdv0103@hotmail.com>
2023-03-19 22:26:46 -0300

    Testando cliques em anuncios

--- prototipo_airbnb.py
+++ prototipo_airbnb.py
@@ -28,21 +28,25 @@ def pagina_pesquisa(cidade, estado, pagina):
 
     return pagina.url
 
-def filtro(pagina_2, url, quartos, camas, banheiros, minimo, maximo, i):
+def filtro(pagina_2, url, quartos, camas, banheiros, minimo, maximo):
 
     pagina_2.goto(str(url))
 
     #FILTRO
     pagina_2.locator('''xpath = /html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div/div/div[1]/div[2]/div/div/div/div/div[2]/div/div/button''').click()
 
-    ##CASA DE HOSPEDES
+    if len(pagina_2.locator('button:has-text("Casa")').all_inner_texts()):
+        ##CASA DE HOSPEDES
+        pagina_2.locator('button:has-text("Casa")').locator('nth =' + repr(1)).click()
+        pagina_2.locator('button:has-text("Casa")').locator('nth =' + repr(2)).click()
+        pagina_2.locator('button:has-text("Apartamento")').click()
 
-    pagina_2.locator('button:has-text("Casa")').locator('nth =' + repr(0)).click()
-    pagina_2.locator('button:has-text("Casa")').locator('nth =' + repr(1)).click()
-    pagina_2.locator('button:has-text("Apartamento")').click()
+    else:
+        pagina_2.locator('button:has-text("Casa")').locator('nth =' + repr(0)).click()
+        pagina_2.locator('button:has-text("Casa")').locator('nth =' + repr(1)).click()
+        pagina_2.locator('button:has-text("Apartamento")').click()
 
 
-    
     pagina_2.locator('button:has-text(' + repr(quartos) + ')').locator('nth =' + repr(-3)).click()
     pagina_2.locator('button:has-text(' + repr(camas) + ')').locator('nth =' + repr(-2)).click()
     pagina_2.locator('button:has-text(' + repr(banheiros) + ')').locator('nth =' + repr(-1)).click() 
@@ -50,11 +54,22 @@ def filtro(pagina_2, url, quartos, camas, banheiros, minimo, maximo, i):
     
     pagina_2.locator('input').locator('nth =' + repr(-19)).fill(repr(minimo))
     pagina_2.locator('input').locator('nth =' + repr(-18)).fill(repr(maximo))
-    time.sleep(10)
-    pagina_2.locator('a:has-text("Mostrar")').click()
+
+    time.sleep(5)
+
+    texto_mostrar = pagina_2.get_by_role("link").all_inner_texts()[1]
+
+    if texto_mostrar.find('Mostrar') != -1:
+        pagina_2.locator('a:has-text("Mostrar")').click()
+
+        pagina_2.locator('article').get_attribute()
 
     pagina_2.close()
 
+
+def clicar_anuncios(texto, pagina_2):
+
+    pagina_2.get_by_role("link").all_inner_texts()
 
 preco_minimo = 1000 #int(input('Preço mínimo: '))
 preco_maximo = 2000 #int(input('Preço máximo: '))
@@ -63,6 +78,7 @@ n_camas = '1' #str(input('Quantas camas: '))
 n_banheiros = '1' #str(input('Quantos banheiros: '))
 
 i = 10
+dif = (preco_maximo - preco_minimo)/i
 
 with sync_playwright() as p:
     for j in range(9):
@@ -76,11 +92,11 @@ with sync_playwright() as p:
 
         pagina_2 = navegador.new_page(viewport = {'width': 1200, 'height': 800})
         
-        dif = (preco_maximo - preco_minimo)/i
-
-        preco_minimo = int(preco_minimo + dif)
+        preco_minimo += int(dif)
 
         filtro(pagina_2, url, n_quartos, n_camas, n_banheiros, preco_minimo, preco_maximo)
+
+        clicar_anuncios(texto, pagina_2)
 
 
 
