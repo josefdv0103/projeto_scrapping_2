99ddb9050dd99f4067ab9c5e92337eef4f490cc9
José Augusto <josefdv0103@hotmail.com>
2023-03-21 23:08:34 -0300

    Implementado o clique nos anuncios no algoritmo

--- prototipo_airbnb.py
+++ prototipo_airbnb.py
@@ -2,6 +2,11 @@ from playwright.sync_api import sync_playwright
 import pandas as pd
 import time
 
+def so_url(string):
+    string =  'https://www.airbnb.com.br' + string
+
+    return string
+
 def pagina_pesquisa(cidade, estado, pagina):
     pagina.goto("https://www.airbnb.com.br/")
 
@@ -55,21 +60,27 @@ def filtro(pagina_2, url, quartos, camas, banheiros, minimo, maximo):
     pagina_2.locator('input').locator('nth =' + repr(-19)).fill(repr(minimo))
     pagina_2.locator('input').locator('nth =' + repr(-18)).fill(repr(maximo))
 
-    time.sleep(5)
-
     texto_mostrar = pagina_2.get_by_role("link").all_inner_texts()[1]
 
     if texto_mostrar.find('Mostrar') != -1:
         pagina_2.locator('a:has-text("Mostrar")').click()
 
-        pagina_2.locator('article').get_attribute()
+        time.sleep(5)
+
+        for i in range(18):
+
+            texto_link = pagina_2.get_by_role('group').get_by_role('link').locator('nth =' + repr(i)).get_attribute('href')
+            
+            link_anuncio = so_url(texto_link)
+
+            pagina_3 = navegador.new_page(viewport = {'width': 1200, 'height': 800})
+
+            pagina_3.goto(link_anuncio)
+
+            pagina_3.close()
 
     pagina_2.close()
-
-
-#def clicar_anuncios(texto, pagina_2):
-
-    
+ 
 
 preco_minimo = 1000 #int(input('Preço mínimo: '))
 preco_maximo = 2000 #int(input('Preço máximo: '))
@@ -89,7 +100,7 @@ with sync_playwright() as p:
         url = pagina_pesquisa('Praia Grande', 'SP', pagina_1)
 
         pagina_1.close()
-
+        
         pagina_2 = navegador.new_page(viewport = {'width': 1200, 'height': 800})
         
         preco_minimo += int(dif)
@@ -97,5 +108,3 @@ with sync_playwright() as p:
         pagina_2.get_by_role("link").all_inner_texts()
 
         filtro(pagina_2, url, n_quartos, n_camas, n_banheiros, preco_minimo, preco_maximo)
-
-    time.sleep(30)
