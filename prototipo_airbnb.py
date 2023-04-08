a8dbdaf3f1620256cc30047e499988f8969a1dac
José Augusto <josefdv0103@hotmail.com>
2023-04-08 15:35:18 -0300

    Reformulando a estrutura e criando exceções

--- prototipo_airbnb.py
+++ prototipo_airbnb.py
@@ -25,7 +25,7 @@ def pagina_pesquisa(cidade, estado, pagina):
     pagina.locator('button:has-text("Buscar")').click()
     return pagina.url
 
-def filtro(pagina_2, url, quartos, camas, banheiros, minimo, maximo, dif, dicionario, frame):
+def filtro(pagina_2, url, quartos, camas, banheiros, minimo, maximo, dif):
 
     pagina_2.goto(str(url))
 
@@ -49,8 +49,8 @@ def filtro(pagina_2, url, quartos, camas, banheiros, minimo, maximo, dif, dicion
     pagina_2.locator('button:has-text(' + repr(banheiros) + ')').locator('nth =' + repr(-1)).click() 
 
     #LIMITES DE PREÇO
-    pagina_2.locator('input').locator('nth =' + repr(-19)).fill(repr(minimo))
-    pagina_2.locator('input').locator('nth =' + repr(-18)).fill(repr(maximo))   
+    pagina_2.locator('section').locator('label').locator('div').locator('div').locator('input').locator('nth = 0').fill(repr(minimo))
+    pagina_2.locator('section').locator('label').locator('div').locator('div').locator('input').locator('nth = 1').fill(repr(maximo))   
 
     #BOTAO MOSTRAR
     texto_mostrar = pagina_2.locator('footer').get_by_role("link").inner_text()
@@ -60,7 +60,7 @@ def filtro(pagina_2, url, quartos, camas, banheiros, minimo, maximo, dif, dicion
 
         maximo = maximo - int(dif)
 
-        pagina_2.locator('input').locator('nth =' + repr(-18)).fill(repr(maximo))
+        pagina_2.locator('section').locator('label').locator('div').locator('div').locator('input').locator('nth = 1').fill(repr(maximo))
 
         pagina_2.mouse.click(1500 * 0.6, 1000 * 0.75)
 
@@ -82,94 +82,75 @@ def filtro(pagina_2, url, quartos, camas, banheiros, minimo, maximo, dif, dicion
 
         texto_mostrar = pagina_2.locator('footer').get_by_role("link").inner_text()
 
+    return texto_mostrar
 
-    #SEPARANDO QUANTAS ABAS EXISTEM BASEADO NOS RESULTADOS
-    pagina_2.locator('footer').get_by_role("link").click()
-    quant_p_abas = 18
-    quant_imoveis = int(texto_mostrar.split()[1])
-    abas = (quant_imoveis // quant_p_abas) + 1
-
-        
-    for i in range(abas):
-
-        if i == range(abas)[-1]:
-            quant_p_abas = (quant_imoveis % quant_p_abas)
-
-        for j in range(1, quant_p_abas):
-
-            texto_link = pagina_2.get_by_role('group').get_by_role('link').locator('nth =' + repr(j)).get_attribute('href')
-            
-            link_anuncio = so_url(texto_link)
-
-            pagina_3 = navegador.new_page(viewport = {'width': 1500, 'height': 1000})
-
-            pagina_3.goto(link_anuncio)
-
-            #CABEÇALHO
-            cabecalho = pagina_3.locator('section').locator('nth = 0').inner_text().split()
-            print(cabecalho)
-
-            ## COMENTÁRIOS
-            dicionario.update({'Nº_de_comentarios' : int(cabecalho[len(pagina_3.locator('h1').inner_text().split()) + 2])})
-            
-            armazenagem(link_anuncio, pagina_3, dicionario, frame, estado, cabecalho)
-
-            pagina_3.close()
-
-    pagina_2.close()
- 
-def armazenagem(link, pagina_1, dicionario, frame, estado, cabecalho):
+def armazenagem(link, pagina_1, dicionario, tabela_2, estado, cabecalho):
 
    #LINK
-    tabela_1.update({'Link' : link})
+    dicionario.update({'link' : link})
 
     #NOTA
-    tabela_1.update({'Avaliacao' : float(pagina_1.locator('''xpath = /html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[1]/div[1]/div/div/div/div/section/div[2]/div[1]/span[1]/span[2]''').inner_text().split()[0].replace(',','.'))})    
+    
+    if pagina_1.locator('section').inner_text().count('·') >= 2:
+        dicionario.update({'avaliacao' : float(pagina_1.locator('''xpath = /html/body/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[1]/main/div/div[1]/div[1]/div[1]/div/div/div/div/section/div[2]/div[1]/span[1]/span[2]''').inner_text().split()[0].replace(',','.'))})    
     
     if 'Superhost' in cabecalho:
         ##SUPERHOST
-        tabela_1.update({'Superhost' : 'Sim'})
+        dicionario.update({'superhost' : 'Sim'})
         ##CIDADE/BAIRRO
-        tabela_1.update({'Localizacao' : ' '.join(cabecalho[(len(pagina_1.locator('h1').inner_text().split()) + 8) : -(3 + len(estado))])})
+        dicionario.update({'localizacao' : pagina_1.locator('div').locator('span').locator('button').locator('span:has-text("Brasil")').inner_text()})
     else:
         ##SUPERHOST
-        tabela_1.update({'Superhost' : 'Nao'})
+        dicionario.update({'superhost' : 'Nao'})
         ##CIDADE/BAIRRO
-        tabela_1.update({'Localizacao' : ' '.join(cabecalho[(len(pagina_1.locator('h1').inner_text().split()) + 5) : -(3 + len(estado))])})
+        dicionario.update({'localizacao' : pagina_1.locator('div').locator('span').locator('button').locator('span:has-text("Brasil")').inner_text()})
 
     #HOSPEDES
-    tabela_1.update({'Hospedes' : int(pagina_1.locator('li').locator('span:has-text("hóspedes")').inner_text().split()[0])})
+    if 'Mais' in pagina_1.locator('li').locator('span:has-text("hóspedes")').inner_text().split():
+        dicionario.update({'hospedes' : int(pagina_1.locator('li').locator('span:has-text("hóspedes")').inner_text().split()[2])})
+        
+    else:    
+        dicionario.update({'hospedes' : int(pagina_1.locator('li').locator('span:has-text("hóspedes")').inner_text().split()[0])})
     
     #QUARTOS
-    tabela_1.update({'Quartos' : int(pagina_1.locator('li').locator('span:has-text("quarto")').inner_text().split()[0])})
-
-    #CAMAS
-    tabela_1.update({'Camas' : int(pagina_1.locator('li').locator('span:has-text("cama")').inner_text().split()[0])})
-
-    #BANHEIROS
-    tabela_1.update({'Banheiros' : int(pagina_1.locator('li').locator('span:has-text("banheiro")').inner_text().split()[0])})
+    dicionario.update({'quartos' : int(pagina_1.locator('li').locator('span:has-text("quarto")').inner_text().split()[0])})
     
+    #BANHEIROS
+    dicionario.update({'banheiros' : int(pagina_1.locator('li').locator('span:has-text("banheiro")').inner_text().split()[0])})
+    
+    #CAMAS
+    dicionario.update({'camas' : int(pagina_1.locator('li').locator('span:has-text("cama")').inner_text().split()[0])})
+
+    #QUANT NOITES
+    dicionario.update({'quant_noites' : int(pagina_1.locator('section').locator('span').locator('div').locator('button').locator('div:has-text("noite")').inner_text().split()[-2])})
+
+    #RESERVAS POR NOITE
+    #if float(pagina_1.locator('section').locator('span').locator('div').locator('button').locator('div:has-text("noite")').inner_text().split()[0].replace(',','.').replace('R$','')) < 10:
+    dicionario.update({'reserva_p_noite' : int(pagina_1.locator('section').locator('span').locator('div').locator('button').locator('div:has-text("noite")').inner_text().split()[0].replace('.','').replace('R$',''))})
+    #else:   
+    #    dicionario.update({'reserva_p_noite' : int(pagina_1.locator('section').locator('span').locator('div').locator('button').locator('div:has-text("noite")').inner_text().split()[0].replace(',','.').replace('R$',''))})
+
     #VALOR TOTAL
-    if float(pagina_1.locator('span:has-text("R$")').locator('nth = 1').inner_text().split()[-1].replace(',','.').replace('R$','')) <= 10:
+    if float(pagina_1.locator('section').locator('div').locator('div').locator('span').locator('span:has-text("R$")').locator('nth  = 1').inner_text().split()[-1].replace(',','.').replace('R$','')) <= 10:
         
-        tabela_1.update({'Total_sem_impostos' : float(pagina_1.locator('span:has-text("R$")').locator('nth = 1').inner_text().split()[-1].replace(',','.').replace('R$',''))*1000})   
+        dicionario.update({'total_sem_impostos' : float(pagina_1.locator('section').locator('div').locator('div').locator('span').locator('span:has-text("R$")').locator('nth  = 1').inner_text().split()[-1].replace(',','.').replace('R$',''))*1000})   
     else:
-        tabela_1.update({'Total_sem_impostos' : float(pagina_1.locator('span:has-text("R$")').locator('nth = 1').inner_text().split()[-1].replace(',','.').replace('R$',''))})   
+        dicionario.update({'total_sem_impostos' : float(pagina_1.locator('section').locator('div').locator('div').locator('span').locator('span:has-text("R$")').locator('nth  = 1').inner_text().split()[-1].replace(',','.').replace('R$',''))})   
 
-    frame = pd.concat([frame, pd.DataFrame([dicionario])], ignore_index = True)
+    tabela_2 = pd.concat([tabela_2, pd.DataFrame([dicionario])], ignore_index = True)
 
-    return frame.to_excel('tabela.xlsx')
+    return tabela_2
 
-tabela_1 = {'Link' : [None], 'Avaliacao' : [None], 'Nº_de_comentarios' : [None], 'Superhost' : [None], 'Localizacao' : [None], 
-            'Hospedes' : [None], 'Hospedagem' : [None], 'Quartos' : [None], 'Banheiros' : [None], 'Camas': [None], 'Taxa_limpeza' : [None], 
-            'Taxa_servico': [None], 'Total_sem_impostos': [None]}
+tabela_1 = {'link' : [None], 'avaliacao' : [None], 'nº_de_comentarios' : [None], 'superhost' : [None], 'localizacao' : [None], 
+            'hospedes' : [None],  'quartos' : [None], 'banheiros' : [None], 'camas': [None], 'reserva_p_noite' : [None], 'quant_noites' : [None], 
+            'taxa_limpeza' : [None], 'taxa_servico': [None], 'total_sem_impostos': [None]}
 
 tabela_2 = pd.DataFrame(tabela_1)
 
 cidade = 'Praia Grande' #str(input('Cidade: '))
 estado = 'São Paulo' #str(input('Estado: '))
 preco_minimo = 1000 #int(input('Preço mínimo: '))
-preco_maximo = 2000 #int(input('Preço máximo: '))
+preco_maximo = 1500 #int(input('Preço máximo: '))
 n_quartos = '1' #str(input('Quantos quartos: '))
 n_camas = '1' #str(input('Quantas camas: '))
 n_banheiros = '1' #str(input('Quantos banheiros: '))
@@ -189,6 +170,41 @@ with sync_playwright() as p:
     pagina_1.close()
     
     pagina_2 = navegador.new_page(viewport = {'width': 1200, 'height': 800})
+    
+    texto_mostrar = filtro(pagina_2, url, n_quartos, n_camas, n_banheiros, preco_minimo, preco_maximo, dif)
 
-    filtro(pagina_2, url, n_quartos, n_camas, n_banheiros, preco_minimo, preco_maximo, dif, tabela_1, tabela_2)
+    #SEPARANDO QUANTAS ABAS EXISTEM BASEADO NOS RESULTADOS
+    pagina_2.locator('footer').get_by_role("link").click()
+    quant_p_abas = 18
+    quant_imoveis = int(texto_mostrar.split()[1])
+    abas = (quant_imoveis // quant_p_abas) + 1
 
+    for i in range(abas):
+
+        if i == range(abas)[-1]:
+            quant_p_abas = (quant_imoveis % quant_p_abas)
+
+        for j in range(1, quant_p_abas):
+
+            texto_link = pagina_2.get_by_role('group').get_by_role('link').locator('nth =' + repr(j)).get_attribute('href')
+            
+            link_anuncio = so_url(texto_link)
+
+            pagina_3 = navegador.new_page(viewport = {'width': 1500, 'height': 1000})
+
+            pagina_3.goto(link_anuncio)
+
+            #CABEÇALHO
+            cabecalho = pagina_3.locator('section').locator('nth = 0').inner_text().split()
+
+            ## COMENTÁRIOS
+            if 'comentários' in cabecalho:
+                tabela_1.update({'nº_de_comentarios' : int(pagina_3.locator('button').locator('span:has-text("comentários")').locator("nth = 1").inner_text().split()[0])})
+            
+            tabela_2 = armazenagem(link_anuncio, pagina_3, tabela_1, tabela_2, estado, cabecalho)
+
+            tabela_2.to_excel('tabela.xlsx')
+
+            pagina_3.close()
+
+    pagina_2.close()
